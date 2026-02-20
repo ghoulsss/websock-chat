dc5144d105310e7a41094bd5c3925c1e194a9896
Danil <danpavkzn@mail.ru>
2026-02-20 10:00:38 +0300

    micro fixes

--- src/api/v1/chat.py
+++ src/api/v1/chat.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from src.dependencies.auth import get_current_user
+from src.services.auth import AuthService
 from src.db.models.users import User
 
 router = APIRouter(prefix="/chat", tags=["Chat"])
@@ -9,7 +9,7 @@ templates = Jinja2Templates(directory="src/templates")
 
 
 @router.get("/", response_class=HTMLResponse, summary="Chat Page")
-async def get_chat_page(request: Request, user_data: User = Depends(get_current_user)):
+async def get_chat_page(request: Request, user_data: User = Depends(AuthService.get_current_user)):
     # users_all = await UsersDAO.find_all()
     return templates.TemplateResponse(
         "chat.html", {"request": request, "user": user_data, "users_all": "asd"}
--- src/services/auth.py
+++ src/services/auth.py
@@ -1,5 +1,10 @@
-from fastapi import Depends
+from datetime import datetime, timezone
 
+from fastapi import Request, HTTPException, status, Depends
+from jose import jwt, JWTError
+
+from src.core.config import get_auth_data
+from src.exceptions.auth import TokenExpiredException, NoUserIdException, TokenNoFoundException, NoJwtException
 from src.exceptions.auth import IncorrectEmailOrPasswordException
 from src.db.repositories.user import UserRepository
 from src.exceptions.auth import UserAlreadyExistsException
@@ -39,4 +44,36 @@ class AuthService:
         user = await self._user_repository.get_user_by_email(email)
         if not user or not verify_password(password, user.hashed_password):
             return None
+        return user
+
+    @staticmethod
+    def get_token(request: Request):
+        token = request.cookies.get("users_access_token")
+        if not token:
+            raise TokenNoFoundException
+        return token
+
+    async def get_current_user(self, token: str = Depends(get_token)):
+        try:
+            auth_data = get_auth_data()
+            payload = jwt.decode(
+                token, auth_data["secret_key"], algorithms=auth_data["algorithm"]
+            )
+        except JWTError:
+            raise NoJwtException
+
+        expire: str = payload.get("exp")
+        expire_time = datetime.fromtimestamp(int(expire), tz=timezone.utc)
+        if (not expire) or (expire_time < datetime.now(timezone.utc)):
+            raise TokenExpiredException
+
+        user_id: str = payload.get("sub")
+        if not user_id:
+            raise NoUserIdException
+
+        user = await self._user_repository.get_user_by_id(int(user_id))
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found"
+            )
         return user
